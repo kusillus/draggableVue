b48e9084b77b31e4c9c392234cadb83bd204da74
Alexander Guevara <wguevarab7@gmail.com>
2017-09-05 17:45:10 -0500

    modificanciones

--- src/App.vue
+++ src/App.vue
@@ -18,30 +18,48 @@
           class="elements"
           @click="showInfo()">
           <div v-for="(item, index) in listone" :key="index">
-            <div class="element" @click="showItem(item)" :id="item.id">{{item.name}},{{index}}</div>
+            <div class="element" @click="showItem(item)" :data-name="item.name" :id="item.id">{{item.name}},{{index}}</div>
           </div>
         </draggable>
         <button @click="showInfo(listone)">SendListOne</button>
       </div>
-      <div class="card">
-        <h4>List 2</h4>
+      <div>
+        <h5 style="margin:0;">Lista Definitiva</h5>
         <draggable
-          :list="listtwo"
-          v-model='listtwo'
-          :options="{group:'items'}"
+          :list="listAn"
+          v-model='listAn'
           :move="checkMove"
+          :options="{group:'items'}"
           @add="onAdd"
-          @end="endDrag"
           class="elements">
-          <div v-for="(item, index) in listtwo" :key="index">
-            <div class="element" @click="showItem(item)" :id="item.id">{{item.name}} , {{index}} <span class="close" @click="removeItem(index)"><strong>x</strong></span></div>
+          <div class="elements" style="margin:.4rem;" v-for="(item, index) in listAn" :key="index">
+            <div v-if="item.name != 'new_list'">
+              <h5 style="margin:0;">Lista {{item.name}}</h5>
+              <div v-for="(itm, idx) in item.values" :key="idx">
+                <draggable
+                  :list="item.values"
+                  v-model='item.values'
+                  :move="moveItem"
+                  @end="endDrag"
+                  :options="{group:'items'}">
+                  <div style="padding:.5rem;" @click="showItem(itm)" :id="itm.id" :data-name="itm.name">{{itm.name}} , {{idx}} <span class="close" @click="removeItemAn(idx, index)"><strong>x</strong></span></div>
+                </draggable>
+              </div>  
+            </div>
+            <div v-else>
+              <h5 style="margin:0;">Lista {{item.name}}</h5>
+              <draggable
+                :list="item.values"
+                v-model='item.values'
+                :options="{group:'items'}"
+                @add="addNew"
+                class="elements bg-red"> 
+              </draggable>
+            </div>
           </div>
         </draggable>
-        <button @click="showInfo(listtwo)">SendListTwo</button>
       </div>
     </div>
-
-  
   </div>
 </template>
 
@@ -61,29 +79,63 @@ export default {
       listtwo: [
         {name: 'item 5', id: "ID5"},
         {name: 'item 6', id: "ID6"}
+      ],
+      listAn: [
+        { name: 'new_list',
+          values: [{}]
+        },
+        { name: 'List1',
+          values: [
+            {name: 'item UNO', id: "ID7"},
+            {name: 'item DOS', id: "ID8"}
+          ]  
+        },
+        { name: 'List2',
+          values: [
+            {name: 'item TRES', id: "ID9"},
+            {name: 'item CUATRO', id: "ID10"}
+          ]  
+        },
+        { name: 'List3',
+          values: [
+            {name: 'item CINCO', id: "ID11"},
+            {name: 'item SEIS', id: "ID12"}
+          ]  
+        }
       ]
     }
   },
-  // watch: {
-  //   listtwo: {
-  //     handler: function(newValue, oldValue){
-  //       // this.diffArray(newValue, oldValue)
-  //     },
-  //     deep: true
-  //   }
-  // },
   methods: {
     removeItem: function (index) {
       this.listtwo.splice(index, 1)
     },
+    removeItemAn: function (idx, supIdx) {
+      if(this.listAn[supIdx].values.length == 1) {
+        this.listAn.splice(supIdx,1)
+      } else {
+        this.listAn[supIdx].values.splice(idx, 1)
+      }
+    },
+    moveItem: function () {
+      return false
+    },
     showInfo: function (elem) {
-      // console.log(elem)
+      console.log(elem)
+      elem.map(function(obj){
+        let newArray = {}
+        if(obj.name == 'new_list'){
+          console.log('no se agrega al nuevo array')
+        } else {
+          newArray.push(obj)
+        }
+        console.log(newArray)
+      })
     },
     showItem: function (elem) {
       // console.log(elem)
     },
     checkMove: function(evt) {
-      // console.log(evt.draggedContext.element.id)
+      console.log(evt)
     },
     endDrag: function(evt) {
       if(evt.oldIndex !== evt.newIndex){
@@ -94,16 +146,24 @@ export default {
         console.log('se mantiene la posicion')
       }
     },
+    pack: function () {
+      console.log('paaack')
+    },
     startDrag: function (evt) {
 			// console.log(evt)
 		},
     onAdd: function (evt) {
-      if(confirm('Desea agregar?') == true){
-        console.log(evt.clone.firstChild.id)
-      } else {
-        console.log('no se agrego')
-        this.removeItem(evt.newIndex)
-      }
+      this.listAn.splice(evt.newIndex, 1)
+    },
+    addNew: function (el) {
+      console.log(el)
+      console.log('ADDNWE!')
+      this.listAn.push({
+        name: 'Lista ' + el.clone.firstChild.dataset.name,
+        values: [
+            {name: el.clone.firstChild.dataset.name, id: el.clone.firstChild.id}
+          ]
+      })
     }
   },
   components: {
@@ -146,9 +206,6 @@ a {
 }
 .card {
   width: 150px;
-  // border: solid .1rem gray;
-  // border-radius: .2rem;
-  // padding: 1rem; 
 }
 .elements {
   cursor: pointer;
@@ -161,7 +218,6 @@ a {
   padding: .5rem .2rem;
 }
 .close {
-  // border: solid .1rem gray;
   border-radius: .2rem;
   background-color: gray;
   color: white;
@@ -170,5 +226,8 @@ a {
 h4 {
   margin: 0;
   text-align: left;
+}
+.bg-red {
+  background-color: red;
 }
 </style>
